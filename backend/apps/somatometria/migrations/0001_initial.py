1a9577716dee89dfb5b861b10dd4d8c45cbd1d7b
victorhak <victorhak@outlook.es>
2026-02-17 12:00:05 -0600

    feat(recepcion): add visit initial schema and external mysql docker setup

--- backend/apps/somatometria/migrations/0001_initial.py
+++ backend/apps/somatometria/migrations/0001_initial.py
@@ -5,7 +5,9 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies = []
+    dependencies = [
+        ("recepcion", "0001_initial"),
+    ]
 
     operations = [
         migrations.CreateModel(
