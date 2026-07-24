58960e5df965c9ced5a653b57b3bdcc80ba1e0d8
Tepexicuapan3 <tepexicuapandavid3@gmail.com>
2026-07-24 12:17:59 -0600

    Configuracion de  las urls para las rutas de django

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django-admin/', admin.site.urls),
     path('api/v1/', include('apps.administracion.urls')),
     path('api/v1/', include('apps.catalogos.urls')),
     path('api/v1/', include('apps.authentication.urls')),
